c5e558de08c1dac8a7966065ba2005aaced1d8e2
Stephanossss <gauranga2005@gmail.com>
2025-04-27 11:08:55 +0300

    Initial commit

---
+++ Assets/player camera.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // Прячем курсор
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Ограничиваем поворота камеры 
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.parent.Rotate(Vector3.up * mouseX); // вращение
+    }
+}
